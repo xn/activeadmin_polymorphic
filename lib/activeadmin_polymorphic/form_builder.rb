1870593ede962d26f32144dc7a397f32905fa389
Christian Trosclair <xn@users.noreply.github.com>
2017-11-09 08:44:05 -0600

    Match current changes in activeadmin

--- lib/activeadmin_polymorphic/form_builder.rb
+++ lib/activeadmin_polymorphic/form_builder.rb
@@ -1,128 +1,194 @@
+# Provides an intuitive way to build has_many associated records in the same form.
+module Formtastic
+  module Inputs
+    module Base
+      def input_wrapping(&block)
+        html = super
+        template.concat(html) if template.output_buffer && template.assigns[:polymorphic_has_many_block]
+        html
+      end
+    end
+  end
+end
 module ActiveadminPolymorphic
   class FormBuilder < ::ActiveAdmin::FormBuilder
     def polymorphic_has_many(assoc, poly_name, options = {}, &block)
-      custom_settings = :new_record, :allow_destroy, :heading, :sortable, :sortable_start, :types, :path_prefix
-      builder_options = {new_record: true, path_prefix: :admin}.merge! options.slice  *custom_settings
+      PolymorphicHasManyBuilder.new(self, assoc, poly_name, options).render(&block)
+    end
+  end
 
-      options         = {for: assoc      }.merge! options.except *custom_settings
-      options[:class] = [options[:class], "polymorphic_has_many_fields"].compact.join(' ')
-      sortable_column = builder_options[:sortable]
-      sortable_start  = builder_options.fetch(:sortable_start, 0)
+  class PolymorphicHasManyBuilder < SimpleDelegator
+     attr_reader :assoc
+     attr_reader :options
+     attr_reader :heading, :sortable_column, :sortable_start
+     attr_reader :new_record, :destroy_option
+     attr_reader :poly_name, :types, :path_prefix
 
-      html = "".html_safe
-      html << template.capture do
-        contents = "".html_safe
+    def initialize(has_many_form, assoc, poly_name, options)
+      super has_many_form
+      @assoc = assoc
+      @options = extract_custom_settings!(options.dup)
+      @options.reverse_merge!(for: assoc)
+      @options[:class] = [options[:class], "polymorphic_has_many_fields"].compact.join(' ')
 
-        block = polymorphic_form(poly_name, builder_options)
-
-        template.assign('polymorphic_has_many_block' => true)
-        contents = without_wrapper { inputs(options, &block) }
-
-        if builder_options[:new_record]
-          contents << js_for_polymorphic_has_many(
-            assoc, poly_name, template, builder_options, options[:class]
-          )
-        else
-          contents
-        end
+      if sortable_column
+        @options[:for] = [assoc, sorted_children(sortable_column)]
       end
+      @poly_name = poly_name
+    end
 
-      tag = @already_in_an_inputs_block ? :li : :div
-      html = template.content_tag(tag, html, class: "polymorphic_has_many_container #{assoc}", 'data-sortable' => sortable_column, 'data-sortable-start' => sortable_start)
-      template.concat(html) if template.output_buffer
-      html
+    def render(&block)
+      html = "".html_safe
+      html << template.content_tag(:h3) { heading } if heading.present?
+      html << template.capture { content_polymorphic_has_many(&block) }
+      wrap_div_or_li(html)
     end
 
     protected
 
-    def polymorphic_has_many_actions(has_many_form, builder_options, contents)
-      if has_many_form.object.new_record?
+    # remove options that should not render as attributes
+    def extract_custom_settings!(options)
+      @heading = options.key?(:heading) ? options.delete(:heading) : default_heading
+      @sortable_column = options.delete(:sortable)
+      @sortable_start  = options.delete(:sortable_start) || 0
+      @new_record = options.key?(:new_record) ? options.delete(:new_record) : true
+      @destroy_option = options.delete(:allow_destroy)
+      @types = options.delete(:types)
+      @path_prefix = options.delete(:path_prefix) || :admin
+      options
+    end
+
+    def default_heading
+      assoc_klass.model_name.
+        human(count: ::ActiveAdmin::Helpers::I18n::PLURAL_MANY_COUNT)
+    end
+
+    def assoc_klass
+      @assoc_klass ||= __getobj__.object.class.reflect_on_association(assoc).klass
+    end
+
+    def content_polymorphic_has_many(&block)
+      form_block = proc do |form_builder|
+        render_polymorphic_has_many_form(form_builder, options[:parent], &block)
+      end
+
+      template.assigns[:polymorphic_has_many_block] = true
+      contents = without_wrapper { inputs(options, &form_block) }
+      contents ||= "".html_safe
+
+      js = new_record ? js_for_polymorphic_has_many(assoc, poly_name, template, options[:class], &form_block) : ''
+      contents << js
+    end
+
+    # Renders the Formtastic inputs then appends ActiveAdmin delete and sort actions.
+    def render_polymorphic_has_many_form(form_builder, parent, &block)
+      index = parent && form_builder.send(:parent_child_index, parent)
+      form_builder.input("#{poly_name}_id", as: :hidden)
+
+      if form_builder.object.send(poly_name).nil?
+        form_builder.input("#{poly_name}_type", input_html: { class: 'polymorphic_type_select' }, as: :select, collection: polymorphic_options)
+      else
+        form_builder.input(
+          "#{poly_name}_type", as: :hidden,
+          input_html: {"data-path" =>  form_edit_path(form_builder.object.send(poly_name)) }
+        )
+      end
+
+      template.concat template.capture { yield(form_builder, index) }
+      template.concat polymorphic_has_many_actions(form_builder, "".html_safe)
+    end
+
+    def polymorphic_has_many_actions(form_builder, contents)
+      if form_builder.object.new_record?
         contents << template.content_tag(:li) do
           template.link_to I18n.t('active_admin.has_many_remove'),
             "#", class: 'button polymorphic_has_many_remove'
         end
-      elsif builder_options[:allow_destroy]
-        contents << has_many_form.input(:_destroy, as: :boolean,
+      elsif allow_destroy?(form_builder.object)
+        contents << form_builder.input(:_destroy, as: :boolean,
                             wrapper_html: {class: 'polymorphic_has_many_delete'},
                             label: I18n.t('active_admin.has_many_delete'))
       end
 
-      if builder_options[:sortable]
-        contents << has_many_form.input(builder_options[:sortable], as: :hidden)
+      if sortable_column
+        form_builder.input sortable_column, as: :hidden
 
         contents << template.content_tag(:li, class: 'handle') do
-          '''
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" width="30px" height="25px" viewBox="0 0 550 550" enable-background="new 0 0 550 550" xml:space="preserve">
-            <path d="M418.909,456.728h-93.091v-288l-93.091,93.091V139.637L372.363,0L512,139.637v122.182l-93.091-93.091V456.728z   M93.091,84.363h93.091v288l93.091-93.091v122.182L139.637,541.091L0,401.454V279.272l93.091,93.091V84.363z"></path>
-          </svg>
-          '''.html_safe
+          I18n.t('active_admin.move')
         end
       end
 
       contents
     end
 
-    def js_for_polymorphic_has_many(assoc, poly_name, template, builder_options, class_string)
-      new_record = builder_options[:new_record]
-      assoc_reflection = object.class.reflect_on_association assoc
-      assoc_name       = assoc_reflection.klass.model_name
+    def allow_destroy?(form_object)
+      !! case destroy_option
+         when Symbol, String
+           form_object.public_send destroy_option
+         when Proc
+           destroy_option.call form_object
+         else
+           destroy_option
+         end
+    end
+
+    def sorted_children(column)
+      __getobj__.object.public_send(assoc).sort_by do |o|
+        attribute = o.public_send column
+        [attribute.nil? ? Float::INFINITY : attribute, o.id || Float::INFINITY]
+      end
+    end
+
+    def without_wrapper
+      is_being_wrapped = already_in_an_inputs_block
+      self.already_in_an_inputs_block = false
+
+      html = yield
+
+      self.already_in_an_inputs_block = is_being_wrapped
+      html
+    end
+
+    def js_for_polymorphic_has_many(assoc, poly_name, template, class_string, &form_block)
+      assoc_name       = assoc_klass.model_name
       placeholder      = "NEW_#{assoc_name.to_s.underscore.upcase.gsub(/\//, '_')}_RECORD"
-
-      text = new_record.is_a?(String) ? new_record : I18n.t('active_admin.has_many_new', model: assoc_name.human)
-      form_block = polymorphic_form(poly_name, builder_options, true)
-
       opts = {
-        for: [assoc, assoc_reflection.klass.new],
+        for: [assoc, assoc_klass.new],
         class: class_string,
         for_options: { child_index: placeholder }
       }
-
-      html = "".html_safe
-      html << template.capture do
-        inputs_for_nested_attributes opts, &form_block
-      end
+      html = template.capture{ __getobj__.send(:inputs_for_nested_attributes, opts, &form_block) }
+      text = new_record.is_a?(String) ? new_record : I18n.t('active_admin.has_many_new', model: assoc_name.human)
 
       template.link_to text, '#', class: "button polymorphic_has_many_add", data: {
         html: CGI.escapeHTML(html).html_safe, placeholder: placeholder
       }
     end
 
-    def polymorphic_options(builder_options)
+    def wrap_div_or_li(html)
+      template.content_tag(already_in_an_inputs_block ? :li : :div,
+                           html,
+                           class: "polymorphic_has_many_container #{assoc}",
+                           'data-sortable' => sortable_column,
+                           'data-sortable-start' => sortable_start)
+    end
+
+    def polymorphic_options
       # add internationalization
-      builder_options[:types].each_with_object([]) do |model, options|
+      types.each_with_object([]) do |model, options|
         options << [
           model.model_name.human, model,
-          {"data-path" => form_new_path(model, builder_options) }
+          {"data-path" => form_new_path(model) }
         ]
       end
     end
 
-    def polymorphic_form(poly_name, builder_options, for_js = false)
-      proc do |f|
-        html = "".html_safe
-        html << f.input("#{poly_name}_id", as: :hidden)
-
-        if f.object.send(poly_name).nil?
-          html << f.input("#{poly_name}_type", input_html: { class: 'polymorphic_type_select' }, as: :select, collection: polymorphic_options(builder_options))
-        else
-          html << f.input(
-            "#{poly_name}_type", as: :hidden,
-            input_html: {"data-path" =>  form_edit_path(f.object.send(poly_name), builder_options) }
-          )
-        end
-
-        html << polymorphic_has_many_actions(f, builder_options, "".html_safe)
-
-        html
-      end
+    def form_new_path(object)
+      "/#{path_prefix}/#{ActiveModel::Naming.plural(object)}/new"
     end
 
-    def form_new_path(object, builder_options)
-      "/#{builder_options[:path_prefix]}/#{ActiveModel::Naming.plural(object)}/new"
-    end
-
-    def form_edit_path(object, builder_options)
-      "/#{builder_options[:path_prefix]}/#{ActiveModel::Naming.plural(object)}/#{object.id}/edit"
+    def form_edit_path(object)
+      "/#{path_prefix}/#{ActiveModel::Naming.plural(object)}/#{object.id}/edit"
     end
   end
 end
